940bfe7a68463ea2245b2476a47b29e3e792e9e1
Cauã <104581895+Mendescaua@users.noreply.github.com>
2026-05-10 16:44:29 -0300

    Update supabse_client.dart

--- lib/core/supabse_client.dart
+++ lib/core/supabse_client.dart
@@ -2,9 +2,9 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 Future<void> initSupabase() async {
   await Supabase.initialize(
-    url: 'https://gqkjdevencvibiqvislk.supabase.co',
+    url: '',
     anonKey:
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imdxa2pkZXZlbmN2aWJpcXZpc2xrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNDMwOTcsImV4cCI6MjA2NDYxOTA5N30.S9MEekdNFugTMOV5xAmdP6xWrVFpPwB-kN_Iwv0KOXY',
+        '',
   );
 }
 
